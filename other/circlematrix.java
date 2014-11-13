3c446d66b43d91567bf99ca2e1b1bdc888b644ca
Lambros Petrou <lambrospetrou@gmail.com>
2014-11-13 20:04:49 +0000

    refactored the next() method to hold the invariant that each time prev holds the next position

--- other/circlematrix.java
+++ other/circlematrix.java
@@ -1,5 +1,7 @@
 /**
- * "Write a function that prints a matrix N x N size and the values are increasing in a spiral order."
+ * "Write a function that for a given Integer N > 0, prints a matrix N x N
+ * size whom the values are increasing in a spiral order."
+ *  e.g N = 4
  * 	1  2  3  4
  * 	12 13 14 5
  * 	11 16 15 6
@@ -9,6 +11,12 @@
 public class circlematrix {
 
     public static class Point {
+        public Point() {
+            x = 0; y = 0;
+        }
+        public Point(int x, int y) {
+            this.x = x; this.y = y;
+        }
         public int x,y;
     }
 
@@ -17,8 +25,8 @@ public class circlematrix {
      * It is like an iterator that moves in spiral order starting from 0,0.
      */
     public static class PointCalc {
-        Point p;
-        int N, dirX = 1, dirY = 0, currentCircle = 0, currentCircleMoves = 0;
+        Point prev;
+        int N, n, dirX = 1, dirY = 0, currentCircle = 0, currentCircleMoves = 0;
 
         // this determines if the next step of X or Y should increment,
         // decrement or stay the same based on the dirX or dirY.
@@ -27,31 +35,38 @@ public class circlematrix {
 
         public PointCalc(int N) {
             this.N = N;
-            p = new Point();
-            p.x = -1;
-            p.y = 0;
+            n = N;
+            prev = new Point();
+            prev.x = 0;
+            prev.y = 0;
         }
 
         // calculate the next position
         Point next() {
-            if (currentCircleMoves == (2*N + 2*(N-2))) {
-                // we finished the current circle
-                currentCircleMoves = 0;
-                N -= 2;
-                currentCircle++;
-                p.x = currentCircle - 1;
-                p.y = currentCircle;
+            if (N == 1) {
+                return new Point(0,0);
             }
+            Point p = new Point(prev.x, prev.y);
             currentCircleMoves++;
-            // check if we have a valid cell by forwarding
-            if ( (p.x + dirs[dirX]) == currentCircle + N || (p.y + dirs[dirY]) == currentCircle +  N
-                    || (p.x + dirs[dirX]) < currentCircle || (p.y + dirs[dirY]) < currentCircle ) {
-                // next rotation
-                dirX = (dirX + 1) % dirs.length;
-                dirY = (dirY + 1) % dirs.length;
+            // if this is the last one of the current circle move
+            // to the next circle inside the spiral
+            if (currentCircleMoves == (2*n + 2*(n-2))) {
+                currentCircleMoves = 0;
+                n -= 2;
+                currentCircle++;
+                prev.x = currentCircle;
+                prev.y = currentCircle;
+            } else {
+                // check if we have a valid cell by forwarding
+                if ((prev.x + dirs[dirX]) == currentCircle + n || (prev.y + dirs[dirY]) == currentCircle + n
+                        || (prev.x + dirs[dirX]) < currentCircle || (prev.y + dirs[dirY]) < currentCircle) {
+                    // next direction
+                    dirX = (dirX + 1) % dirs.length;
+                    dirY = (dirY + 1) % dirs.length;
+                }
+                prev.x = prev.x + dirs[dirX];
+                prev.y = prev.y + dirs[dirY];
             }
-            p.x = p.x + dirs[dirX];
-            p.y = p.y + dirs[dirY];
             return p;
         }
     }
