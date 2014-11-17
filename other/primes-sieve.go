2616c098ab897adf7c52cf4790a67e51976d7641
Lambros Petrou <lambrospetrou@gmail.com>
2014-11-17 19:08:00 +0000

    added another function that uses atomics instead of channels with slight speedup

--- other/primes-sieve.go
+++ other/primes-sieve.go
@@ -6,10 +6,11 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"sync/atomic"
 	"time"
 )
 
-func insertionSort(arr []int, length *int, elem int, N int) {
+func insertionSort(arr []int64, length *int64, elem int64, N int64) {
 	i := *length - 1
 	for arr[i] > elem {
 		arr[i+1] = arr[i]
@@ -23,46 +24,169 @@ func insertionSort(arr []int, length *int, elem int, N int) {
 	}
 }
 
-func primesConc1(N int, procs int) int {
-	defer timeStop(time.Now(), "Primes Concurrent 1 finished")
+func primesConc2(N int64, procs int64) int64 {
+	defer timeStop(time.Now(), "Primes Concurrent 2 finished")
 
-	primesFound := make([]int, N+1)
+	primesFound := make([]int64, N+1)
 	primesFound[0] = 2
 	primesFound[1] = 3
 	primesFound[2] = 5
 	primesFound[3] = 7
+	primesStored := int64(4)
 
 	// receives all the valid primes from the workers
-	chPrimes := make(chan int, procs<<2)
-	chAllPrimesInserted := make(chan int, procs<<2)
-	chNextCandidate := make(chan int, procs<<4)
+	chPrimes := make(chan int64, procs<<2)
+
+	var gAllPrimesInserted int64 = 4
+	var gNextCandidate int64 = 1
 
 	chProcChannels := make(chan bool, procs)
-
-	// candidates generator
-	go func() {
-		for i := 1; ; i++ {
-			chAllPrimesInserted <- i
-		}
-	}()
-
-	primesStored := 4
 
 	done := make(chan bool, procs)
 	allDone := make(chan bool, 1)
 
 	gatherPrimes := func() {
-		var p int
-		var terminated int = 0
+		var p int64
+		var terminated int64 = 0
 		for terminated < procs {
 			select {
 			case <-done:
 				terminated++
 			case p = <-chPrimes:
-				var clen int = primesStored
+				var clen int64 = primesStored
 				insertionSort(primesFound, &primesStored, p, N)
 				if clen == N-1 {
-					for i := 0; i < procs; i++ {
+					for i := int64(0); i < procs; i++ {
+						chProcChannels <- true
+					}
+				}
+			}
+		}
+		allDone <- true
+		fmt.Println("all workers exited")
+		// all workers finished so just check their remaining results
+		for {
+			select {
+			case p = <-chPrimes:
+				insertionSort(primesFound, &primesStored, p, N)
+			default:
+				return
+			}
+		}
+	}
+
+	// returns True if the worker can continue finding more primes
+	// or False if it should exit
+	checkPrime := func(next int64) int64 {
+		var finished bool = false
+		cPrime := int64(3)
+		nextSqrt := int64(math.Sqrt(float64(next)))
+		//fmt.Println("checking: ", nextSqrt, next)
+		// check for prime validity against the found primes so far
+		for j := int64(0); j < primesStored; j++ {
+			cPrime = primesFound[j]
+			if cPrime > nextSqrt {
+				finished = true
+				break
+			} else if next%cPrime == 0 {
+				return -1
+			}
+		}
+		if finished == false {
+			// now we have to check for possible divisors
+			// larger than the primes we found so far but less than SQRT
+			// of our num since other workers might be checking them
+			// and they still haven't added them to the found primes
+			for cPrime <= nextSqrt {
+				if next%cPrime == 0 {
+					return -1
+				}
+				cPrime += 2
+			}
+		}
+		// we have a valid prime number
+		chPrimes <- next
+		//return <-chAllPrimesInserted
+		return atomic.AddInt64(&gAllPrimesInserted, 1)
+	}
+
+	for i := int64(0); i < procs; i++ {
+		go func(procChannelFinishedN chan bool) {
+			var seq1, nextCandidate int64
+			for {
+				// make sure there are primes to find
+				select {
+				case <-procChannelFinishedN:
+					fmt.Println("exiting - notified by main - last candidate:",
+						nextCandidate)
+					done <- true
+					return
+				default:
+					// just proceed
+				}
+
+				//nextCandidate = <-chNextCandidate
+				nextCandidate = atomic.AddInt64(&gNextCandidate, 1)
+				nextCandidate = nextCandidate*6 - 1
+
+				if seq1 = checkPrime(nextCandidate); seq1 >= N {
+					fmt.Println("exiting", seq1, nextCandidate)
+					done <- true
+					return
+				}
+
+				if seq1 = checkPrime(nextCandidate + 2); seq1 >= N {
+					fmt.Println("exiting", seq1, nextCandidate+2)
+					done <- true
+					return
+				}
+			}
+		}(chProcChannels)
+	}
+
+	gatherPrimes()
+	return primesFound[N-1]
+}
+
+func primesConc1(N int64, procs int64) int64 {
+	defer timeStop(time.Now(), "Primes Concurrent 1 finished")
+
+	primesFound := make([]int64, N+1)
+	primesFound[0] = 2
+	primesFound[1] = 3
+	primesFound[2] = 5
+	primesFound[3] = 7
+	primesStored := int64(4)
+
+	// receives all the valid primes from the workers
+	chPrimes := make(chan int64, procs<<2)
+	chAllPrimesInserted := make(chan int64, procs<<2)
+	chNextCandidate := make(chan int64, procs<<4)
+
+	chProcChannels := make(chan bool, procs)
+
+	// candidates generator
+	go func() {
+		for i := int64(primesStored); ; i++ {
+			chAllPrimesInserted <- i
+		}
+	}()
+
+	done := make(chan bool, procs)
+	allDone := make(chan bool, 1)
+
+	gatherPrimes := func() {
+		var p int64
+		var terminated int64 = 0
+		for terminated < procs {
+			select {
+			case <-done:
+				terminated++
+			case p = <-chPrimes:
+				var clen int64 = primesStored
+				insertionSort(primesFound, &primesStored, p, N)
+				if clen == N-1 {
+					for i := int64(0); i < procs; i++ {
 						chProcChannels <- true
 					}
 				}
@@ -83,7 +207,7 @@ func primesConc1(N int, procs int) int {
 
 	// candidates generator
 	go func() {
-		for i := 2; ; i++ {
+		for i := int64(2); ; i++ {
 			select {
 			case chNextCandidate <- i:
 			case <-allDone:
@@ -94,13 +218,13 @@ func primesConc1(N int, procs int) int {
 
 	// returns True if the worker can continue finding more primes
 	// or False if it should exit
-	checkPrime := func(next int) int {
+	checkPrime := func(next int64) int64 {
 		var finished bool = false
-		cPrime := 3
-		nextSqrt := int(math.Sqrt(float64(next)))
+		cPrime := int64(3)
+		nextSqrt := int64(math.Sqrt(float64(next)))
 		//fmt.Println("checking: ", nextSqrt, next)
 		// check for prime validity against the found primes so far
-		for j := 0; j < primesStored; j++ {
+		for j := int64(0); j < primesStored; j++ {
 			cPrime = primesFound[j]
 			if cPrime > nextSqrt {
 				finished = true
@@ -126,9 +250,9 @@ func primesConc1(N int, procs int) int {
 		return <-chAllPrimesInserted
 	}
 
-	for i := 0; i < procs; i++ {
+	for i := int64(0); i < procs; i++ {
 		go func(procChannelFinishedN chan bool) {
-			var seq1, nextCandidate int
+			var seq1, nextCandidate int64
 			for {
 				// make sure there are primes to find
 				select {
@@ -203,5 +327,6 @@ func main() {
 	N, _ := strconv.Atoi(os.Args[1])
 
 	fmt.Printf("Result: %d\n", primesSerial(N))
-	fmt.Printf("Result: %d\n", primesConc1(N, runtime.NumCPU()))
+	fmt.Printf("Result: %d\n", primesConc1(int64(N), int64(runtime.NumCPU())))
+	fmt.Printf("Result: %d\n", primesConc2(int64(N), int64(runtime.NumCPU())))
 }
