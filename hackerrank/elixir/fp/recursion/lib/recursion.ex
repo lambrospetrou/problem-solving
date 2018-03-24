c9f0fa1b5ea78b3f906247cdf6f5643dc2abbb42
Lambros Petrou <lambrospetrou@gmail.com>
2018-03-24 22:30:56 +0000

    Add Pascal triangle calculation

--- hackerrank/elixir/fp/recursion/lib/recursion.ex
+++ hackerrank/elixir/fp/recursion/lib/recursion.ex
@@ -1,10 +1,16 @@
 defmodule Recursion.Gcd do
   def main() do
-    [x, y] = IO.gets("") |> String.trim |> String.split(" ", parts: 2) |> Enum.map(&String.to_integer/1)
-    gcd(x, y) |> IO.puts
+    [x, y] =
+      IO.gets("")
+      |> String.trim()
+      |> String.split(" ", parts: 2)
+      |> Enum.map(&String.to_integer/1)
+
+    gcd(x, y) |> IO.puts()
   end
 
   def gcd(x, 0), do: x
+
   def gcd(x, y) do
     gcd(y, Integer.mod(x, y))
   end
@@ -12,19 +18,46 @@ end
 
 defmodule Recursion.Fibonacci do
   def main() do
-    n = IO.gets("") |> String.trim |> String.to_integer
-    fib(n) |> IO.puts
+    n = IO.gets("") |> String.trim() |> String.to_integer()
+    fib(n) |> IO.puts()
   end
 
   def fib_naive(1), do: 0
   def fib_naive(2), do: 1
-  def fib_naive(n), do: fib(n-1) + fib(n-2)
+  def fib_naive(n), do: fib(n - 1) + fib(n - 2)
 
   def fib(1), do: 0
   def fib(2), do: 1
   def fib(n), do: do_fib(n, 0, 1)
   def do_fib(3, prev_2, prev_1), do: prev_2 + prev_1
-  def do_fib(n, prev_2, prev_1), do: do_fib(n-1, prev_1, prev_1 + prev_2)
+  def do_fib(n, prev_2, prev_1), do: do_fib(n - 1, prev_1, prev_1 + prev_2)
+end
+
+defmodule Recursion.Pascal do
+  def main() do
+    n = IO.gets("") |> String.trim() |> String.to_integer()
+
+    triangle(n)
+    |> Enum.reverse()
+    |> Enum.map(fn line ->
+      Enum.join(line, " ") |> IO.puts()
+    end)
+  end
+
+  def triangle(1), do: [[1]]
+
+  def triangle(n) do
+    top = triangle(n - 1)
+    previous = top |> List.first()
+
+    summed =
+      Enum.zip(previous, Enum.drop(previous, 1))
+      |> Enum.map(fn {a, b} -> a + b end)
+
+    row = [1 | summed] ++ [1]
+
+    [row | top]
+  end
 end
 
 defmodule Recursion do
--- hackerrank/elixir/fp/recursion/test/recursion_test.exs
+++ hackerrank/elixir/fp/recursion/test/recursion_test.exs
@@ -12,21 +12,29 @@ defmodule RecursionTest do
   end
 
   test "fibonacci" do
-  	alias Recursion.Fibonacci
-  	assert Fibonacci.fib(1) == 0
-  	assert Fibonacci.fib(2) == 1
-  	assert Fibonacci.fib(3) == 1
-  	assert Fibonacci.fib(4) == 2
-  	assert Fibonacci.fib(5) == 3
-  	assert Fibonacci.fib(6) == 5
-  	assert Fibonacci.fib(7) == 8
-  	assert Fibonacci.fib(8) == 13
-  	assert Fibonacci.fib(9) == 21
-  	assert Fibonacci.fib(10) == 34
-  	assert Fibonacci.fib(11) == 55
-  	assert Fibonacci.fib(12) == 89
-  	assert Fibonacci.fib(13) == 144
-  	assert Fibonacci.fib(14) == 233
-  	assert Fibonacci.fib(15) == 377
+    alias Recursion.Fibonacci
+    assert Fibonacci.fib(1) == 0
+    assert Fibonacci.fib(2) == 1
+    assert Fibonacci.fib(3) == 1
+    assert Fibonacci.fib(4) == 2
+    assert Fibonacci.fib(5) == 3
+    assert Fibonacci.fib(6) == 5
+    assert Fibonacci.fib(7) == 8
+    assert Fibonacci.fib(8) == 13
+    assert Fibonacci.fib(9) == 21
+    assert Fibonacci.fib(10) == 34
+    assert Fibonacci.fib(11) == 55
+    assert Fibonacci.fib(12) == 89
+    assert Fibonacci.fib(13) == 144
+    assert Fibonacci.fib(14) == 233
+    assert Fibonacci.fib(15) == 377
+  end
+
+  test "pascal triangle" do
+    alias Recursion.Pascal
+    assert Pascal.triangle(1) == [[1]]
+    assert Pascal.triangle(2) == [[1, 1], [1]]
+    assert Pascal.triangle(3) == [[1, 2, 1], [1, 1], [1]]
+    assert Pascal.triangle(4) == [[1, 3, 3, 1], [1, 2, 1], [1, 1], [1]]
   end
 end
