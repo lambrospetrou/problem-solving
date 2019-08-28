f8dc7f25beea26ad8033e1996bfc753e22d81415
Lambros Petrou <lambrospetrou@gmail.com>
2019-08-28 20:51:24 +0100

    Add specific folder for daily coding problems

---
+++ daily-coding-problem/javascript/src/p0080.test.ts
@@ -0,0 +1,36 @@
+import { deepestNode } from "./p0080"
+
+it("should return 'd'", () => {
+  const tree = {
+    left: {
+      left: {
+        value: 'd'
+      },
+      value: 'b'
+    },
+    right: {
+      value: 'c',
+      left: {
+        value: 'e',
+        right: {
+          value: 'f',
+          left: {
+            value: 'h'
+          }
+        },
+        left: {
+          value: 'g'
+        }
+      }
+    },
+    value: 'a'
+  };
+
+  expect(deepestNode(tree).value).toEqual('h');
+  expect(deepestNode(tree.left).value).toEqual('d');
+  expect(deepestNode(tree.right).value).toEqual('h');
+});
+
+it('should return the root if no children', () => {
+  expect(deepestNode({value: 'a'}).value).toEqual('a');
+});
